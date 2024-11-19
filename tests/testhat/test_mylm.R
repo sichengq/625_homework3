cfe301622bcb1c157659bf6c78f06bb9d8a1da0d
sichengq <147861284+sichengq@users.noreply.github.com>
2024-11-19 18:09:21 -0500

    Update test_mylm.R

--- tests/testhat/test_mylm.R
+++ tests/testhat/test_mylm.R
@@ -1,1 +1,26 @@
+test_that("my_lm fits a basic linear model", {
+  data(mtcars)
+  fit <- my_lm(mpg ~ wt + cyl, data = mtcars)
 
+  # Check class
+  expect_s3_class(fit, "lm")
+
+  # Check coefficients
+  expect_length(fit$coefficients, 3)  # Intercept, wt, cyl
+
+  # Check residuals length
+  expect_equal(length(fit$residuals), nrow(mtcars))
+
+  # Check names of coefficients
+  expect_equal(names(fit$coefficients), c("(Intercept)", "wt", "cyl"))
+})
+
+test_that("my_lm handles missing data correctly", {
+  data(mtcars)
+  mtcars$mpg[1] <- NA
+
+  fit <- my_lm(mpg ~ wt + cyl, data = mtcars, na.action = na.omit)
+
+  # Check that missing row is omitted
+  expect_equal(length(fit$residuals), nrow(mtcars) - 1)
+})
